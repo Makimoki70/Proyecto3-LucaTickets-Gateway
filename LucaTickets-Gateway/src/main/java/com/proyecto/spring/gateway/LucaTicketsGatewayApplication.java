67c816befd64beb0944901cb476639e7b0643503
Alberto <makimoki69@gmail.com>
2022-12-02 10:39:14 +0100

    Añadido microservicio Gateway

---
+++ LucaTickets-Gateway/src/main/java/com/proyecto/spring/gateway/LucaTicketsGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.proyecto.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LucaTicketsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaTicketsGatewayApplication.class, args);
+	}
+
+}
